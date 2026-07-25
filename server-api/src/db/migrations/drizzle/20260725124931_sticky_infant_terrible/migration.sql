962d9ccd25011e7c5220c16b8782fad24bc1f663
harshawije97 <harshawije99@gmail.com>
2026-07-25 20:04:45 +0530

    migrations are completed

--- server-api/src/db/migrations/drizzle/20260725124931_sticky_infant_terrible/migration.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE "auth" (
-	"auth_id" uuid PRIMARY KEY DEFAULT gen_random_uuid(),
-	"email" varchar(255) NOT NULL,
-	"password" varchar(255) NOT NULL,
-	"refresh_token" varchar(500) NOT NULL,
-	"created_at" timestamp DEFAULT now() NOT NULL,
-	"updated_at" timestamp
-);
--- server-api/src/db/schema.db.ts
+++ server-api/src/db/schema.db.ts
@@ -1,16 +1,39 @@
 import { timestamp, uuid, varchar } from 'drizzle-orm/pg-core/columns';
 import { pgTable } from 'drizzle-orm/pg-core/table';
+import { roles } from './enums/db.enums';
+
+export { roles } from './enums/db.enums';
 
 // Users schema
-export const users = pgTable('auth', {
-  id: uuid('auth_id').defaultRandom().primaryKey(),
-  email: varchar('email', { length: 255 }).notNull(),
-  password: varchar('password', { length: 255 }).notNull(),
-  refreshToken: varchar('refresh_token', { length: 500 }).notNull(),
+export const users = pgTable('users', {
+  id: uuid('id').defaultRandom().primaryKey(),
+  firstName: varchar('first_name', { length: 255 }).notNull(),
+  lastName: varchar('last_name', { length: 255 }).notNull(),
+  email: varchar('email', { length: 255 }).unique().notNull(),
+  role: roles('roles').default('user').notNull(),
   createdAt: timestamp('created_at').defaultNow().notNull(),
   updatedAt: timestamp('updated_at'),
 });
 
-// Export user types
 export type User = typeof users.$inferSelect;
 export type NewUser = typeof users.$inferInsert;
+
+// Auth schema
+export const auth = pgTable('auth', {
+  id: uuid('id').defaultRandom().primaryKey(),
+  userId: uuid('user_id')
+    .notNull()
+    .unique()
+    .references(() => users.id, { onDelete: 'cascade' }),
+  password: varchar('password', { length: 255 }).notNull(),
+  refreshToken: varchar('refresh_token', { length: 500 }),
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+  updatedAt: timestamp('updated_at')
+    .defaultNow()
+    .notNull()
+    .$onUpdate(() => new Date()),
+});
+
+// Export user types
+export type Auth = typeof auth.$inferSelect;
+export type NewAuth = typeof auth.$inferInsert;
